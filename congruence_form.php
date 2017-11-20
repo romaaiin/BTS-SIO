0694f7149e1d407693902cf0e3f755abac94dfd6
romaaiin <33833273+romaaiin@users.noreply.github.com>
2017-11-20 15:42:28 +0100

    Update congruence_form.php

--- congruence_form.php
+++ congruence_form.php
@@ -1,6 +1,35 @@
-<?php include("entete.html"); ?>
+<!DOCTYPE html>
+<html lang="fr">
 
-		<body>
+	<head>
+		<title>HTML</title>
+		<meta http-equiv="Content-Language"content="fr"/>
+		<meta charset="utf-8" />
+		<meta author="Romain" />
+		<link type="text/css" media="screen" href="./css/global.css" rel="stylesheet" />
+		<style type="text/css" media="screen">
+	
+
+
+		</style>
+			<h1>Gerlache Romain - Gréta Viva5 - Iveco Bus Annonay</h1>
+		
+			<ul id="navbas">
+				<form id="form_rib" action="rib.php" method="POST">
+					<li> <a href="index.php">Accueil</a> </li>
+					<li> <a href="fichestagiaire.php"> Fiche Stagiaire </a> </li>
+					<li> <a href="ficheentreprise.php"> Fiche entreprise </a> </li>
+					<li> <a href="contact.php"> Contacts </a> </li> <br>
+					<li> <a href="renseignements.php"> Demande de renseignements </a> </li>
+					<li> <a href="div-form.html">Division euclidienne</a></li>
+					<li> <a href="congruence_form.php">Vérification Congruence</a></li>
+					<li> <a href="rib_form.php">Vérification Code banque</a></li>
+				</form>
+			</ul>
+			
+	</head>
+
+	<body>
 				<h1>Vérification Congruence</h1>
 				
 			
